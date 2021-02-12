1238c52f45abcb78e5d6cce838103bfb0e4d59dc
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-02-12 04:47:25 +0100

    initial commit

---
+++ src/test/java/com/lekwacious/poll/PollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekwacious.poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
